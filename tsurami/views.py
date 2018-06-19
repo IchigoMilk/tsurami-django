61f6b0a4c0037fd7379be5e9ee83302d7ca2d50d
IchigoMilk <socket65536@gmail.com>
2018-06-20 00:07:46 +0900

    add bs4

--- tsurami/views.py
+++ tsurami/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class PostIndexView(generic.TemplateView):
+class PostIndexView(LoginRequiredMixin, generic.TemplateView):
   template_name = 'tsurami/index.html'
 
   def get_context_data(self, **kwargs):
--- tsurami_prj/settings.py
+++ tsurami_prj/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'tsurami.apps.TsuramiConfig',
     'reset_migrations',
     'django.contrib.admin',
@@ -122,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 # ログイン後トップページにリダイレクト
 LOGIN_REDIRECT_URL = '/'
