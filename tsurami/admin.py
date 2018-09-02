1585f0bfc6d18fe3a0f139534d31638f91ff2c4b
IchigoMilk <socket65536@gmail.com>
2018-09-03 00:11:36 +0900

    change pkey int to uuid

--- tsurami/admin.py
+++ tsurami/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
-from .models import Note, Comment
+from .models import Note, Page
 
 admin.site.register(Note)
-admin.site.register(Comment)
+admin.site.register(Page)
--- tsurami/models.py
+++ tsurami/models.py
@@ -1,21 +1,25 @@
+import uuid
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
 class Note(models.Model):
     """ノート"""
-    note_id     = models.PositiveIntegerField(default=0)
+    note_id     = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created_at  = models.DateTimeField('created-date', default=timezone.now)
 
-    # def __str__(self):
-    #     return self.note_id
+    def __str__(self):
+        return str(self.note_id)
 
-class Comment(models.Model):
+class Page(models.Model):
     """コメント"""
-    message_id  = models.PositiveIntegerField(default=0)
+    page_id     = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created_at  = models.DateTimeField('created-at', default=timezone.now)
     text        = models.CharField('text', max_length=500)
     note        = models.ForeignKey(
         Note,
-        on_delete=models.PROTECT,
+        on_delete=models.CASCADE,
     )
+
+    def __str__(self):
+        return str(self.page_id)
